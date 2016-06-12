f3725cf828e1b0a6ee96d21f2310397628ccdea8
Adolfo Silva <a9126@alunos.ipca.pt>
2016-06-12 21:40:21 +0100

    Processes are confusing...

--- geometry.erl
+++ geometry.erl
@@ -1,7 +1,18 @@
 -module(geometry).
--export([area/1]).
+-export([area/0, area/1]).
 
 % Calculates the area of a shape
 area({square, Side}) -> Side * Side;
 area({rectangle, Width, Height}) -> Width * Height;
 area({circle, Radius}) -> Pi = 3.14159, Pi * Radius * Radius.
+
+area() ->
+  receive
+    {From, {square, X}} ->
+       From ! {self(), X * X};
+    {From, {rectangle, X, Y}} ->
+       From ! {self(), X * Y};
+    {From, {circle, R}} ->
+       From ! {self(), 3.14159 * R * R}
+  end,
+  area().
